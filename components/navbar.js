674b85fea5d239c896939d2eccbd5722e0dca762
ayaznoori <ayaz.noori15@gmail.com>
2022-05-02 11:56:53 +0530

    test

---
+++ components/navbar.js
@@ -0,0 +1,7 @@
+function navbar(){
+    return `<a href="index.html">Home</a>
+    <input type="text" id="search_input" placeholder="Search news" onkeypress="inputdata(event)">`
+}
+
+
+export { navbar }
---
+++ scripts/news.js
@@ -0,0 +1,3 @@
+// Ude Import export (MANDATORY)
+import { navbar  } from '/components/navbar.js';
+document.getElementById("navbar").innerHTML=navbar();
